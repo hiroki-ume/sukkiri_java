7f01df1b552459d128d60d58d00da84387d70f04
hiroki-ume <h.ume1219bknb@gmail.com>
2020-08-06 18:10:05 +0900

    [first commit]

---
+++ chapter1/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        int age;
+        age = 30;
+        System.out.println(age);
+
+        System.out.println("----------------");
+
+        final double PI = 3.14;
+        int pie = 5;
+        System.out.println("半径" + pie + "cmのパイの面積は、");
+        System.out.println(pie * pie * PI);
+        System.out.println("パイの半径を半分にします");
+        PI = 10;
+        System.out.println("半径" + pie + "cmのパイの面積は、");
+        System.out.println(pie * pie + PI);
+    }
+}
